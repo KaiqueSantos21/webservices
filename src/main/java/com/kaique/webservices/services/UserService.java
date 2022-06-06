72eb5fd7ce92f0fb6e5e13aaef3bfe456b9414aa
Kaique Santos <kaiqueosantos@gmail.com>
2022-06-06 11:12:37 -0300

    Exception handling - findById

--- src/main/java/com/kaique/webservices/services/UserService.java
+++ src/main/java/com/kaique/webservices/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.kaique.webservices.entities.User;
 import com.kaique.webservices.repositories.UserRepository;
+import com.kaique.webservices.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
